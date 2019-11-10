48b6aa8134a6996a842143738bdddef1d398881f
mickmason <mmason2@hotmail.com>
2019-11-10 19:13:03 +0000

    Aborted go at adding eslint formatting

--- watcher.js
+++ watcher.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 fs.watch('./app', {
 		recursive: true	
 	}, (eventType, filename) => {
-		console.log(`--watcher.js: ${eventType}`);	
+		console.log(`${(new Date())} --watcher.js: ${eventType}`);	
 		console.log(`--watcher.js: ${filename}`);
 });
