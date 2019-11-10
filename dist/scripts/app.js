01fc4310190bc4f27bd40b80eccbbbaa88019729
mickmason <mmason2@hotmail.com>
2019-11-10 20:28:08 +0000

    Restructuring watch task to have html and js subtasks

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,9 +1,8 @@
 /* App scripts */ 
 let a = 22;
 console.log(`Hi ${a}`);
-function myfunkey(a) {
+function myfunkey(a) { 
 	return a + 1; 
 }
-
 console.log(`${ a }`);
 console.log(`${ myfunkey(23) }`);
